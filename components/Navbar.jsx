132697faf8ed239aec7cc234e513f0aed2d20add
TheKing0000 <simon.kovesdi2@gmail.com>
2022-08-30 11:31:13 +0200

    scrollable navbar on mobile

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,31 +5,33 @@ import { AiOutlineMenu, AiOutlineClose, AiOutlineMail } from "react-icons/ai"
 import { FaLinkedinIn, FaGithub } from "react-icons/fa"
 import { BsFillPersonLinesFill } from "react-icons/bs"
 const Navbar = () => {
+
   const [nav, setNav] = useState(false);
   const handleNav = () => {
     setNav(!nav)
   }
+
   return (
-    <div className='fixed w-full h-20 shadow-xl z-[100]'>
-      <div className='flex justify-between items-center w-full h-full  px-2 2xl:px-16'>
+    <div className='fixed w-full h-16 shadow-2xl z-[100]'>
+      <div className='flex justify-between items-center w-full h-full  px-2 2xl:px-16 '>
 
         <Image width="125" height="50" src="/assets/navLogo.png" alt="nav logo" />
         <div >
-          <ul className='md:mx-7 hidden md:flex'>
+          <ul className='hidden md:mx-7 md:flex'>
             <Link href="/">
-              <li className='ml-10 text-sm uppercase hover:border-b'>Home</li>
+              <li className='ml-10 text-sm uppercase hover:scale-105 '>Home</li>
             </Link>
             <Link href="/">
-              <li className='ml-10 text-sm uppercase hover:border-b'>About</li>
+              <li className='ml-10 text-sm uppercase hover:scale-105'>About</li>
             </Link>
             <Link href="/">
-              <li className='ml-10 text-sm uppercase hover:border-b'>Skills</li>
+              <li className='ml-10 text-sm uppercase hover:scale-105'>Skills</li>
             </Link>
             <Link href="/">
-              <li className='ml-10 text-sm uppercase hover:border-b'>Projects</li>
+              <li className='ml-10 text-sm uppercase hover:scale-105'>Projects</li>
             </Link>
             <Link href="/">
-              <li className='ml-10 text-sm uppercase hover:border-b'>Contact</li>
+              <li className='ml-10 text-sm uppercase hover:scale-105'>Contact</li>
             </Link>
           </ul>
           <div onClick={handleNav} className='mr-5 md:hidden'>
@@ -37,11 +39,12 @@ const Navbar = () => {
           </div>
         </div>
       </div>
-      <div className={nav ? " md:hidden fixed left-0 top-0 w-full h-screen bg-black/70 " : ""}>
+      {/*Mobil nézeten miután megnyomod a hamburger menüt */}
+      <div className={nav ? "md:hidden fixed left-0 top-0 w-full h-screen bg-black/70" : ""}>
         <div
           className={nav ?
-            ' fixed left-0 top-0 w-[75%] sm:w-[60%]  h-screen bg-[#ecf0f3] p-10 ease-in duration-500'
-            : "fixed left-[-100%] top-0 p-10  ease-in duration-500"}>
+            'overflow-auto fixed left-0 top-0 w-[75%] sm:w-[60%]  h-screen bg-[#ecf0f3] p-10 ease-in duration-500'
+            : "fixed left-[-100%] top-0 p-10  ease-in duration-500 "}>
           <div>
             <div className='flex w-full  justify-between items-center'>
               <Image width="87" height="35" src="/assets/navLogo.png" alt="nav logo" />
@@ -50,7 +53,7 @@ const Navbar = () => {
               </div>
             </div>
             <div className='border-b border-gray-300 my-4'>
-              <p className='w-[85%] md:w-[90%] py-4'>Let&apos;s build something legendary together!</p>
+              <p className='w-[85%]  py-4'>Let&apos;s build something legendary together!</p>
             </div>
           </div>
           <div className='py-4 flex flex-col'>
@@ -88,8 +91,6 @@ const Navbar = () => {
                 <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
                   <BsFillPersonLinesFill />
                 </div>
-
-
               </div>
             </div>
           </div>
