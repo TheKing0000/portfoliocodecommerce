328e6d76cc4db121e528176b605fbe2d89a3a510
TheKing0000 <simon.kovesdi2@gmail.com>
2022-08-30 15:05:23 +0200

    about.js

---
+++ components/Skills.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Skills = () => {
+  return (
+    <div className='w-full lg:h-screen p-2'>
+      <div className='h-full max-w-[1240px] mx-auto flex flex-col justify-center'>
+        <p className='text-xl tracking-widest uppercase'> Skills</p>
+        <h2 className='py-4'>What I Can Do</h2>
+        <div className='grid md:grid-cols-2 lg:grid-cols-4 gap-8'>
+
+
+          <div className='p-6 shadow-xl rounded-xl hover:scale-110 ease-in duration-300'>
+          </div>
+
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Skills
