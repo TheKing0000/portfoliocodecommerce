fbaab4987bafbbbd60297741e18a6b1a70d7c59f
TheKing0000 <simon.kovesdi2@gmail.com>
2022-09-08 22:19:06 +0200

    og custom

--- pages/_document.js
+++ pages/_document.js
@@ -6,17 +6,20 @@ export default function Document() {
       <Head>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
-        <meta property="og:title" content="Im the og title" />
-        <meta property="og:description" content="Im the og description" />
-        <meta property="og:type" content="this is the type" />
+        <meta
+          property="og:title"
+          content="Simon Front End Developer Portfolio Page"
+        />
+        <meta
+          property="og:description"
+          content="I can build for you a modern web application using the best technology stack. I am going to use Next js with wich is Nexflix and Tiktok and other big IT companies were developed. Contact me if you are interested in a collaboration!"
+        />
+        <meta property="og:type" content="website" />
         <meta
           property="og:url"
-          content="https://flaviocopes.com/setup-github-credentials-macos/"
+          content="https://portfoliocodecommerce.vercel.app/"
         />
-        <meta
-          property="og:image"
-          content="https://flaviocopes.com/img/avatar.png"
-        />
+        <meta property="og:image" content="/assets/next.png" />
       </Head>
       <body>
         <Main />
